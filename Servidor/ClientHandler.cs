d49d465d5ce74b209a8bcf04968b70b9b87ac392
Micael Rodrigues <micaelr95@outlook.pt>
2019-05-26 11:39:01 +0100

    Server code

---
+++ Servidor/ClientHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using EI.SI;
+
+namespace Servidor
+{
+    class ClientHandler
+    {
+        private TcpClient client;
+        private int clientId;
+
+        public ClientHandler(TcpClient client, int clientId)
+        {
+            this.client = client;
+            this.clientId = clientId;
+        }
+
+        public void Handle()
+        {
+            // Cria a thread
+            Thread thread = new Thread(ThreadHandler);
+            thread.Start();
+        }
+
+        private void ThreadHandler()
+        {
+            NetworkStream networkStream = client.GetStream();
+
+            ProtocolSI protocolSI = new ProtocolSI();
+
+            // Repete ate receber a mensagem de fim de transmissao
+            while (protocolSI.GetCmdType() != ProtocolSICmdType.EOT)
+            {
+                // Recebe as mensagens do cliente
+                int bytesRead = networkStream.Read(protocolSI.Buffer, 0, protocolSI.Buffer.Length);
+                byte[] ack;
+
+                // Verifica o tipo de mensagem
+                switch (protocolSI.GetCmdType())
+                {
+                    // Se for uma mensagem
+                    case ProtocolSICmdType.DATA:
+                        Console.WriteLine("    (Cliente {0}): {1}", clientId, protocolSI.GetStringFromData());
+
+                        // Envia o ACK para o cliente
+                        ack = protocolSI.Make(ProtocolSICmdType.ACK);
+                        networkStream.Write(ack, 0, ack.Length);
+                        break;
+                    // Se for para fechar a comunicacao
+                    case ProtocolSICmdType.EOT:
+                        Console.WriteLine("O Cliente {0} desconnectou-se", clientId);
+
+                        // Envia o ACK para o cliente
+                        ack = protocolSI.Make(ProtocolSICmdType.ACK);
+                        networkStream.Write(ack, 0, ack.Length);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // Fecha as conecoes
+            networkStream.Close();
+            client.Close();
+        }
+    }
+}
---
+++ Servidor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using EI.SI;
+
+namespace Servidor
+{
+    class Program
+    {
+        private const int PORT = 5050;
+
+        static void Main(string[] args)
+        {
+            // Total de clientes ligados
+            int clientCounter = 0;
+
+            // Ouve por licações tcp
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, PORT);
+            tcpListener.Start();
+
+            Console.WriteLine("Servidor Iniciado...");
+
+            while (true)
+            {
+                // Recebe ligacao do cliente
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                // Incrementa o contador de clientes
+                clientCounter++;
+
+                Console.WriteLine("Clientes Ligados: " + clientCounter.ToString());
+
+                // Instancia uma nova thread para o cliente
+                ClientHandler clientHandler = new ClientHandler(tcpClient, clientCounter);
+
+                clientHandler.Handle();
+            }
+        }
+    }
+}
